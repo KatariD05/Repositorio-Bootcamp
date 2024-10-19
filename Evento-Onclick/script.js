0ab66b3e631d27f6f95679f70fabcf3bc443f7ce
KatariD05 <corteskatari9@gmail.com>
2024-10-19 12:44:44 -0300

    alertas

--- Evento-Onclick/script.js
+++ Evento-Onclick/script.js
@@ -4,13 +4,28 @@ const cambio = (x) => {
     x.innerText = "Cerrar Sesion";
 }
 
-let contador = 0;
+let contador1 = 0;
 
-const meGusta = (element) => {
-    contador++;
-    element.innerText = contador + " me gusta";
+const meGusta1 = (element) => {
+    contador1++;
+    element.innerText = contador1 + " me gusta";
+};
+
+let contador2 = 0;
+
+const meGusta2 = (element) => {
+    contador2++;
+    element.innerText = contador2 + " me gusta";
 };
 
 const hide = (x) => {
     x.remove();
+}
+
+const alerta1 = () => {
+    alert('Gato Atigrado was liked')
+}
+
+const alerta2 = () => {
+    alert('Golden Retriever was liked')
 }
